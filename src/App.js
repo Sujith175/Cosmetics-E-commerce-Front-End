4400a64fc45804fd1e0ac0197682fc1cc42ddde0
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2023-03-20 03:39:23 +0530

    feat:products section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Products from "./Components/Products/Products";
 import Slider from "./Components/Slider/Slider";
+import Virtual from "./Components/Virtual/Virtual";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Hero />
       <Slider />
+      <Virtual />
+      <Products />
     </div>
   );
 }
