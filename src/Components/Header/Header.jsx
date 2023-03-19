05a46ea6b1e15f2db9047d1ce8cb65b57b01bbb7
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2023-03-19 14:47:09 +0530

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import css from "./Header.module.css";
+import Logo from "../../assets/logo.png";
+import { CgShoppingBag } from "react-icons/cg";
+const Header = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={Logo} alt="" />
+        <span>Dr. Swathy's</span>
+      </div>
+      <div className={css.right}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>Eng</li>
+          </ul>
+        </div>
+        <input type="text" placeholder="Search" className={css.search} />
+        <CgShoppingBag className={css.cart} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import css from "./Hero.module.css";
+const Hero = () => {
+  return (
+    <div className={css.container}>
+      {/*left side */}
+      <div className={css.h_sides}>
+        <span>Skin Protection Cream</span>
+        <div className={css.text2}>
+          <span>Trendy Collection</span>
+          <span>
+            Seedly say has suitable disposal and boy. Exercise joy man rejoiced
+          </span>
+        </div>
+      </div>
+      {/*middle side hero image */}
+    </div>
+  );
+};
+
+export default Hero;
