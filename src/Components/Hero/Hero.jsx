a228c5cea11aa63b5ccdb8a6fa931d86dda1a2a3
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2023-03-20 01:34:59 +0530

    feat:slider Completed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,48 @@
 import React from "react";
 import css from "./Hero.module.css";
+import heroImg from "../../assets/hero.png";
+
+import { BsArrowRight } from "react-icons/bs";
+import { RiShoppingBagFill } from "react-icons/ri";
 const Hero = () => {
   return (
     <div className={css.container}>
       {/*left side */}
       <div className={css.h_sides}>
-        <span>Skin Protection Cream</span>
+        <span className={css.text1}>Skin Protection Cream</span>
         <div className={css.text2}>
           <span>Trendy Collection</span>
           <span>
-            Seedly say has suitable disposal and boy. Exercise joy man rejoiced
+            Seedly say has suitable disposal and boy. Exercise joy man rejoiced.
           </span>
         </div>
       </div>
+
       {/*middle side hero image */}
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={heroImg} alt="" width={600} />
+        <div className={css.cart2}>
+          <RiShoppingBagFill />
+          <div className={css.signup}>
+            <span>Best SignUp Offers</span>
+            <div>
+              <BsArrowRight />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*right side */}
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span>1.5M</span>
+          <span>Monthly Traffic</span>
+        </div>
+        <div className={css.customers}>
+          <span>100k</span>
+          <span>Happy Customers</span>
+        </div>
+      </div>
     </div>
   );
 };
